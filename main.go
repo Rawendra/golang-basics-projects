cd12e9594d778ceab6ee9a7c2e711eab70fc814f
rawendra <rawendra@gmail.com>
2024-01-27 01:32:05 +0530

    basics channels and routine

--- main.go
+++ main.go
@@ -12,18 +12,26 @@ func main() {
 		"https://www.primevideo.com",
 		"https://www.youtube.com"}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
+	}
+
+	for index := 0; index < len(links); index++ {
+		fmt.Println(<-c)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "the link is not working it seems")
+		c <- "the link is not working it seems"
 		return
 	}
 	fmt.Println(link, "link is working fine")
+	c <- "link is working fine"
 
 }
